1fd2183b994882faf513dd550746fa44251f1786
ITSUPPORT <itservizi.helpdesk@gmail.com>
2025-06-30 13:06:15 +0300

    Lesson Twenty Four

---
+++ Lesson TwentyFour/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace LessonTwentyFour
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var order = new Order { Id = 101, Amount = 200.00 };
+
+            IPaymentProcessor processor = new PayPalPayment(); 
+            IOrderRepository repository = new OrderRepository();
+
+            var service = new OrderService(processor, repository);
+            service.FinalizeOrder(order);
+        }
+    }
+}
---
+++ Lesson TwentyFour/SrpOcpRefactor.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+    public class Order
+    {
+        public int Id { get; set; }
+        public double Amount { get; set; }
+    }
+
+    public interface IPaymentProcessor
+    {
+        void ProcessPayment(Order order);
+    }
+
+    public class CreditCardPayment : IPaymentProcessor
+    {
+        public void ProcessPayment(Order order)
+        {
+            Console.WriteLine("Processing credit card payment...");
+            
+        }
+    }
+
+    public class PayPalPayment : IPaymentProcessor
+    {
+        public void ProcessPayment(Order order)
+        {
+            Console.WriteLine("Processing PayPal payment...");
+            
+        }
+    }
+
+    public class BankTransferPayment : IPaymentProcessor
+    {
+        public void ProcessPayment(Order order)
+        {
+            Console.WriteLine("Processing bank transfer payment...");
+            
+        }
+    }
+
+    public interface IOrderRepository
+    {
+        void Save(Order order);
+        Order Load(int orderId);
+    }
+
+    public class OrderRepository : IOrderRepository
+    {
+        public void Save(Order order)
+        {
+            Console.WriteLine("Salvarea comenzii în baza de date...");
+            
+        }
+
+        public Order Load(int orderId)
+        {
+            Console.WriteLine("Încărcarea comenzii din baza de date...");
+            return new Order { Id = orderId, Amount = 100.0 }; 
+        }
+    }
+
+    public class OrderService
+    {
+        private readonly IPaymentProcessor _paymentProcessor;
+        private readonly IOrderRepository _orderRepository;
+
+        public OrderService(IPaymentProcessor paymentProcessor, IOrderRepository orderRepository)
+        {
+            _paymentProcessor = paymentProcessor;
+            _orderRepository = orderRepository;
+        }
+
+        public void FinalizeOrder(Order order)
+        {
+            _paymentProcessor.ProcessPayment(order);
+            _orderRepository.Save(order);
+        }
+    }
