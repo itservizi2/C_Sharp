be5e40436c85083b952b7b4da5d5264f4819a7e8
ITSUPPORT <itservizi.helpdesk@gmail.com>
2025-06-30 14:26:48 +0300

    Lesson Twenty Five

---
+++ Lesson TwentyFive/IspLspDipRefactor.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+    public interface IWorkable
+    {
+        void Work();
+    }
+
+    public interface IEatable
+    {
+        void Eat();
+    }
+
+    public class HumanWorker : IWorkable, IEatable
+    {
+        public void Work()
+        {
+            Console.WriteLine("Human working...");
+        }
+
+        public void Eat()
+        {
+            Console.WriteLine("Human eating...");
+        }
+    }
+
+    public class RobotWorker : IWorkable
+    {
+        public void Work()
+        {
+            Console.WriteLine("Robot working...");
+        }
+    }
+
+    public class WorkManager
+    {
+        private readonly IWorkable _worker;
+
+        public WorkManager(IWorkable worker)
+        {
+            _worker = worker;
+        }
+
+        public void Manage()
+        {
+            _worker.Work();
+        }
+    }
+
+    public class EatManager
+    {
+        private readonly IEatable _eater;
+
+        public EatManager(IEatable eater)
+        {
+            _eater = eater;
+        }
+
+        public void Manage()
+        {
+            _eater.Eat();
+        }
+    }
---
+++ Lesson TwentyFive/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace LessonTwentyFive
+{
+    
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IWorkable humanWorker = new HumanWorker();
+            IEatable humanEater = (IEatable)humanWorker;
+
+            IWorkable robotWorker = new RobotWorker();
+
+            var humanWorkManager = new WorkManager(humanWorker);
+            var humanEatManager = new EatManager(humanEater);
+
+            var robotWorkManager = new WorkManager(robotWorker);
+            
+            Console.WriteLine("Managing human:");
+            humanWorkManager.Manage();
+            humanEatManager.Manage();
+
+            Console.WriteLine("\nManaging robot:");
+            robotWorkManager.Manage();
+
+        }
+    }
+}
