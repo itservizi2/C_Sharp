a2a7390a6f3506a69ab4d015626bb2c105a1b935
ITSUPPORT <itservizi.helpdesk@gmail.com>
2025-07-04 15:51:19 +0300

    Lesson TwentySeven

--- LessonTwentySevenXunitOne/UnitTest1.cs
+++ LessonTwentySevenXunitOne/UnitTest1.cs
@@ -1,11 +1,59 @@
 namespace LessonTwentySevenXunitOne
 {
-    public class UnitTest1
-    {
-        [Fact]
-        public void Test1()
-        {
+    using LessonTwentySeven;
+    using Xunit;
 
+    
+        public class StringValidatorTests
+        {
+            [Theory]
+            [InlineData("petre@xstar.com", true)]
+            [InlineData("petre.marinescu@xstar.co", true)]
+            [InlineData("petremarinescu.com", false)]
+            [InlineData("petre@com", false)]
+            public void IsValidEmail_ShouldReturnExpectedResult(string email, bool expected)
+            {
+                var result = StringValidator.IsValidEmail(email);
+                Assert.Equal(expected, result);
+            }
+
+            [Theory]
+            [InlineData("1234567890", true)]
+            [InlineData("987654321", false)]
+            [InlineData("abcdefghij", false)]
+            [InlineData("12345abcde", false)]
+            public void IsPhoneNumber_ShouldReturnExpectedResult(string phoneNumber, bool expected)
+            {
+                var result = StringValidator.IsPhoneNumber(phoneNumber);
+                Assert.Equal(expected, result);
+            }
         }
-    }
+
+        public class StringUtilsTests
+        {
+            private readonly StringUtils _utils = new();
+
+            [Theory]
+            [InlineData("hello", "olleh")]
+            [InlineData("12345", "54321")]
+            [InlineData("a", "a")]
+            [InlineData("", "")]
+            public void Reverse_ShouldReturnReversedString(string input, string expected)
+            {
+                var result = _utils.Reverse(input);
+                Assert.Equal(expected, result);
+            }
+
+            [Theory]
+            [InlineData("Racecar", true)]
+            [InlineData("Never Odd Or Even", true)]
+            [InlineData("Hello", false)]
+            [InlineData("Was it a car or a cat I saw", true)]
+            public void IsPalindrome_ShouldReturnExpectedResult(string input, bool expected)
+            {
+                var result = _utils.IsPalindrome(input);
+                Assert.Equal(expected, result);
+            }
+        }
+    
 }
