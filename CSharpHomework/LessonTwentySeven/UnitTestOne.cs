fabf8f9015a23a6f8fdff84df0d84c71fe1b6ab5
ITSUPPORT <itservizi.helpdesk@gmail.com>
2025-07-04 14:05:35 +0300

    updated Lesson Fourteen

---
+++ CSharpHomework/LessonTwentySeven/UnitTestOne.cs
@@ -0,0 +1,35 @@
+﻿namespace LessonTwentySeven
+{
+    public class StringValidator
+    {
+        public static bool IsValidEmail(string email)
+        {
+            return email.Contains("@") && email.Contains(".");
+        }
+
+        public static bool IsPhoneNumber(string phoneNumber)
+        {
+            return phoneNumber.Length == 10 && phoneNumber.All(char.IsDigit);
+        }
+    }
+
+
+
+    public class StringUtils
+    {
+        public string Reverse(string input)
+        {
+            char[] charArray = input.ToCharArray();
+            Array.Reverse(charArray);
+            return new string(charArray);
+        }
+
+
+        public bool IsPalindrome(string input)
+        {
+            string cleanedInput = input.ToLower().Replace(" ", "");
+            string reversedInput = Reverse(cleanedInput);
+            return cleanedInput == reversedInput;
+        }
+    }
+}
