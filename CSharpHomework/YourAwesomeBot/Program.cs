560731b9106179f9f72453ead39ebcee6242a17c
ITSUPPORT <itservizi.helpdesk@gmail.com>
2025-08-14 23:17:33 +0300

    Removed sensitive content from appsettings.json to user secrets and switched to development mode

--- CSharpHomework/YourAwesomeBot/Program.cs
+++ CSharpHomework/YourAwesomeBot/Program.cs
@@ -12,7 +12,12 @@ public class Program
         var host = Host.CreateDefaultBuilder(args)
             .ConfigureAppConfiguration((context, config) =>
             {
-                config.AddJsonFile("appsettings.json");
+                
+                config.AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+
+                
+                config.AddUserSecrets<Program>();
+                
             })
             .ConfigureServices((context, services) =>
             {
